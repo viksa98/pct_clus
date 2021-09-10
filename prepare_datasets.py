ec85020cdfb47442abcab92f6ce48b164574886d
Viktor Andonovic <andonovic.viksa@gmail.com>
2021-09-10 09:55:17 +0200

    minor fixes

--- prepare_datasets.py
+++ prepare_datasets.py
@@ -3,7 +3,6 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from _preprocessing import *
 from util import *
 from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
