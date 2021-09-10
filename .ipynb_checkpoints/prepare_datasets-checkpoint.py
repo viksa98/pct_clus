d34b3b4742568a650d7b72ada14cbed30f9fb8dd
Viktor Andonovic <andonovic.viksa@gmail.com>
2021-09-10 10:10:45 +0200

    bug fixes

--- .ipynb_checkpoints/prepare_datasets-checkpoint.py
+++ .ipynb_checkpoints/prepare_datasets-checkpoint.py
@@ -8,18 +8,6 @@ from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 
 if __name__ == '__main__':
-
-    data_path = os.getcwd() + '/data/'
-    filename = 'BO_truncated_mso_2018.pcl'
-
-    df = load_hecat_data(path = data_path, filename = filename)
-
-    # df = df[df['truncated']==0]
-#     vektor = df['truncated'].to_numpy()
-    df = drop_unnecessary_columns(df)
-    df = rename_columns(df)
-    df = transform_entry_date(df)
-    df = df[df['duration']>0]
     
     columns_to_keep = ['id', 'truncated', 'Age', 'Months_of_work_experience', 'Entry_month_sin', 'Entry_day_sin',
        'Entry_month_cos', 'Entry_day_cos', 'Gender', 'Education_category', 'Dissabilities',
@@ -28,18 +16,26 @@ if __name__ == '__main__':
 
     continuous = ['Age', 'Months_of_work_experience', 'Entry_month_sin', 'Entry_day_sin',
            'Entry_month_cos', 'Entry_day_cos']
+    
+    cols = ['eApplication', 'Gender']
+    
+    for_scaling = ['Age', 'Months_of_work_experience']
+
+    data_path = os.getcwd() + '/data/'
+    filename = 'BO_truncated_mso_2018.pcl'
+
+    df = load_hecat_data(path = data_path, filename = filename)
+    df = drop_unnecessary_columns(df)
+    df = rename_columns(df)
+    df = transform_entry_date(df)
+    df = df[df['duration']>0]
+    
 
     df = df[columns_to_keep]
     
-#     one_hot_encoded_data = pd.get_dummies(new_df, columns = [column for column in new_df.columns if column not in continuous+['eApplication', 'Gender', 'duration']])
-#     final_df = one_hot_encoded_data.copy()
-    
     le = preprocessing.LabelEncoder()
-    cols = ['eApplication', 'Gender']
     for col in cols:
         df[col] = le.fit_transform(df[col].values)
-        
-    for_scaling = ['Age', 'Months of work experience']
 
     scaler = MinMaxScaler() 
     scaled_values = scaler.fit_transform(df[[col for col in df.columns if col in for_scaling]]) 
@@ -47,6 +43,9 @@ if __name__ == '__main__':
     
     df_test = df.sample(frac=0.2)
     df_train = df.drop(df_test.index)
+    
+    df_train = df_train.set_index('id')
+    df_test = df_test.set_index('id')
     
     df_train.to_csv('train_hecat.csv')
     df_test.to_csv('test_hecat.csv')
