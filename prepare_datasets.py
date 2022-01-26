7278faff88713343075171d5a2eb6050588b8cf3
viksa98 <andonovic.viksa@gmail.com>
2022-01-26 11:03:58 +0100

    fix

--- prepare_datasets.py
+++ prepare_datasets.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     cols = ['eApplication', 'Gender', 'Profession_program']
     for_scaling = ['Age', 'Months_of_work_experience']
 
-    data_path = os.getcwd() + '/data/'
+    data_path = os.getcwd() + '/data'
     filename = 'BO_truncated_mso_2018.pcl'
 
     df = load_hecat_data(path = data_path, filename = filename)
@@ -34,6 +34,9 @@ if __name__ == '__main__':
     for col in cols:
         df[col] = le.fit_transform(df[col].values)
 
+    # df['Profession_program'] = df['Profession_program'].astype('str')
+
+
     scaler = MinMaxScaler() 
     scaled_values = scaler.fit_transform(df[[col for col in df.columns if col in for_scaling]]) 
     df.loc[:,[col for col in df.columns if col in for_scaling]] = scaled_values
@@ -44,6 +47,6 @@ if __name__ == '__main__':
     df_train = df_train.set_index('id')
     df_test = df_test.set_index('id')
     
-    df_train.to_csv('train_hecat.csv')
-    df_test.to_csv('test_hecat.csv')
-    df.to_csv('full_hecat.csv')
+    df_train.to_csv('./train_hecat.csv')
+    df_test.to_csv('./test_hecat.csv')
+    df.to_csv('./full_hecat.csv')
